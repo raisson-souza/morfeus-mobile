a299e39cf08c773fb09b68e07a8bd11e3a354d95
Raisson Souza <raissonrai@gmail.com>
2024-11-06 15:17:38 -0300

    Implementação parcial das telas de login e cadastro

--- src/screens/Stack/Login.tsx
+++ src/screens/Stack/Login.tsx
@@ -2,20 +2,51 @@ import { RouteProp, useNavigation, useRoute } from "@react-navigation/native"
 import { Screen } from "../../components/base/Screen"
 import { StackNavigationParams } from "../../../App"
 import { StackNavigationProp } from "@react-navigation/stack"
-import { StyleSheet, Text } from "react-native"
+import { StyleSheet, Text, TextInput, View } from "react-native"
 import CustomButton from "../../components/CustomButton"
-import React from "react"
+import React, { useState } from "react"
 
 type LoginStackUseNavigationProps = StackNavigationProp<StackNavigationParams, "Login">
 type LoginStackUseRouteProps = RouteProp<StackNavigationParams, "Login">
 
 export const LoginScreen: React.FC<{}> = ({ }) => {
+    // Não é possível acessar LoginScreen quando logado
     const stackNavigation = useNavigation<LoginStackUseNavigationProps>()
     const stackRoute = useRoute<LoginStackUseRouteProps>()
+    const [ credentials, setCredentials ] = useState<{ email: string, password: string }>({ email: "", password: "" })
+
+    const login = () => {
+        // USAR VALIDADOR
+        // SERVICE
+    }
 
     return (
         <Screen>
-            <Text>LOGIN</Text>
+            <Text>Realize seu login</Text>
+            <View style={ styles.container }>
+                <View>
+                    <Text>Email</Text>
+                    <TextInput
+                        placeholder="usuario@email.com"
+                        textContentType="emailAddress"
+                        defaultValue={ credentials.email }
+                        onChangeText={ (e) => setCredentials({ email: e, password: credentials.password })}
+                    />
+                </View>
+                <View>
+                    <Text>Senha</Text>
+                    <TextInput
+                        placeholder="12345"
+                        textContentType="password"
+                        defaultValue={ credentials.password }
+                        onChangeText={ (e) => setCredentials({ email: credentials.email, password: e })}
+                    />
+                </View>
+                <CustomButton
+                    title="Entrar"
+                    onPress={ login }
+                />
+            </View>
             <CustomButton title="Info" onPress={ () => stackNavigation.navigate("Info") } />
             <CustomButton title="Registry" onPress={ () => stackNavigation.navigate("Registry") } />
             <CustomButton title="Tabs" onPress={ () => stackNavigation.navigate("Tabs") } />
@@ -24,4 +55,11 @@ export const LoginScreen: React.FC<{}> = ({ }) => {
 }
 
 const styles = StyleSheet.create({
+    container: {
+        display: "flex",
+        textAlign: "center",
+        textAlignVertical: "center",
+        flexDirection: "column",
+        gap: 10,
+    },
 })
--- src/screens/Stack/Registry.tsx
+++ src/screens/Stack/Registry.tsx
@@ -2,19 +2,64 @@ import { RouteProp, useNavigation, useRoute } from "@react-navigation/native"
 import { Screen } from "../../components/base/Screen"
 import { StackNavigationParams } from "../../../App"
 import { StackNavigationProp } from "@react-navigation/stack"
-import { StyleSheet, Text } from "react-native"
+import { StyleSheet, Text, TextInput, View } from "react-native"
 import CustomButton from "../../components/CustomButton"
-import React from "react"
+import React, { useState } from "react"
 
 type RegistryStackUseNavigationProps = StackNavigationProp<StackNavigationParams, "Registry">
 type RegistryStackUseRouteProps = RouteProp<StackNavigationParams, "Registry">
 
 export const RegistryScreen: React.FC<{}> = ({ }) => {
+    // Não é possível acessar RegistryScreen quando logado
     const stackNavigation = useNavigation<RegistryStackUseNavigationProps>()
     const stackRoute = useRoute<RegistryStackUseRouteProps>()
+    const [ credentials, setCredentials ] = useState<{
+        email: string,
+        password: string,
+        passwordRepeat: string,
+    }>({
+        email: "",
+        password: "",
+        passwordRepeat: "",
+    })
+
+    const register = () => {
+        // USAR VALIDADOR
+        // SERVICE
+    }
 
     return (
         <Screen>
+            <Text>Cadastre-se</Text>
+            <View>
+                <View>
+                    <Text>Email</Text>
+                    <TextInput
+                        placeholder="usuario@email.com"
+                        textContentType="emailAddress"
+                        defaultValue={ credentials.email }
+                        onChangeText={ (e) => setCredentials({ email: e, password: credentials.password, passwordRepeat: credentials.passwordRepeat })}
+                    />
+                </View>
+                <View>
+                    <Text>Senha</Text>
+                    <TextInput
+                        placeholder="12345"
+                        textContentType="password"
+                        defaultValue={ credentials.password }
+                        onChangeText={ (e) => setCredentials({ email: credentials.email, password: e, passwordRepeat: credentials.passwordRepeat })}
+                    />
+                </View>
+                <View>
+                    <Text>Repita a senha</Text>
+                    <TextInput
+                        placeholder="12345"
+                        textContentType="password"
+                        defaultValue={ credentials.passwordRepeat }
+                        onChangeText={ (e) => setCredentials({ email: credentials.email, password: credentials.password, passwordRepeat: e })}
+                    />
+                </View>
+            </View>
             <Text>CADASTRO</Text>
             <CustomButton title="Info" onPress={ () => stackNavigation.navigate("Info") } />
             <CustomButton title="Login" onPress={ () => stackNavigation.navigate("Login") } />
