a40b8f78166de4002f6722285894e53e0537df89
Raisson Souza <raissonrai@gmail.com>
2024-11-12 15:27:05 -0300

    - Implementação de DatePicker e TimePicker; - Métodos de formatação de data em DateFormatter; - Filtros de listagem de sonhos por usuário parcialmente implementados.

--- src/screens/Dreams/DreamsList.tsx
+++ src/screens/Dreams/DreamsList.tsx
@@ -1,15 +1,17 @@
 import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs"
-import { DateTimePickerAndroid, DateTimePickerEvent } from "@react-native-community/datetimepicker"
-import { DreamListedByUserType } from "../../types/dream"
+import { DateFormatter } from "../../utils/DateFormatter"
+import { DreamListedByUserType, ListDreamsByUserRequest } from "../../types/dream"
 import { DreamsStackNavigationParams } from "../Tabs/Dreams"
+import { Picker } from "@react-native-picker/picker"
 import { RouteProp, useNavigation } from "@react-navigation/native"
 import { Screen } from "../../components/base/Screen"
 import { StackNavigationParams, TabNavigationParams } from "../../../App"
 import { StackNavigationProp } from "@react-navigation/stack"
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Switch, Text, View } from "react-native"
 import { SyncContextProvider } from "../../contexts/SyncContext"
 import Auth from "../../components/auth/Auth"
 import CustomButton from "../../components/customs/CustomButton"
+import DatePicker from "../../components/customs/DatePicker"
 import DreamListedByUser from "../../components/dreams/DreamListedByUser"
 import DreamService from "../../services/api/DreamService"
 import Loading from "../../components/base/Loading"
@@ -30,36 +32,38 @@ export const DreamsList: React.FC<DreamsListProps> = ({ route }) => {
     const tabNavigation = useNavigation<DreamsListsTabNavigationProps>()
     const [ dreamsList, setDreamsList ] = useState<DreamListedByUserType[] | null>(null)
     const { isConnectedRef: { current: isOnline }} = SyncContextProvider()
+    const [ date, setDate ] = useState<Date>(new Date())
+    const [ listDreamsByUserForm, setListDreamsByUserForm ] = useState<ListDreamsByUserRequest>({
+        dreamOriginFilter: "all",
+        dreamCaracteristicsFilter: "allNotHiddenAndErotic",
+        dreamEspecificCaracteristicsFilter: {
+            noEspecificy: true,
+            dreamsWithPersonalAnalysis: null,
+            dreamClimates: {
+                ameno: false,
+                calor: false,
+                garoa: false,
+                chuva: false,
+                tempestade: false,
+                nevoa: false,
+                neve: false,
+                multiplos: false,
+                outro: false,
+                indefinido: false,
+            },
+            dreamHourId: null,
+            dreamDurationId: null,
+            dreamLucidityLevelId: null,
+            dreamTypeId: null,
+            dreamRealityLevelId: null,
+            dreamPointOfViewId: null,
+        },
+        date: DateFormatter.forBackend.date(date.getTime())
+    })
 
     useEffect(() => {
         const fetchDreams = async () => {
-            await DreamService.ListByUser(isOnline, {
-                dreamOriginFilter: "all",
-                dreamCaracteristicsFilter: "all",
-                dreamEspecificCaracteristicsFilter: {
-                    noEspecificy: true,
-                    dreamsWithPersonalAnalysis: null,
-                    dreamClimates: {
-                        ameno: false,
-                        calor: false,
-                        garoa: false,
-                        chuva: false,
-                        tempestade: false,
-                        nevoa: false,
-                        neve: false,
-                        multiplos: false,
-                        outro: false,
-                        indefinido: false,
-                    },
-                    dreamHourId: null,
-                    dreamDurationId: null,
-                    dreamLucidityLevelId: null,
-                    dreamTypeId: null,
-                    dreamRealityLevelId: null,
-                    dreamPointOfViewId: null,
-                },
-                date: "2024/11/01"
-            })
+            await DreamService.ListByUser(isOnline, listDreamsByUserForm)
             .then(response => {
                 if (response.Success) {
                     setDreamsList(response.Data)
@@ -69,38 +73,53 @@ export const DreamsList: React.FC<DreamsListProps> = ({ route }) => {
             })
         }
         fetchDreams()
-    }, [])
-
-    const [ date, setDate ] = useState<Date>(new Date())
-
-    const onChange = (event: DateTimePickerEvent, selectedDate?: Date) => {
-        const currentDate = selectedDate
-        if (currentDate) setDate(currentDate)
-    }
-
-    const showMode = (currentMode: "date" | "time") => {
-        DateTimePickerAndroid.open({
-            value: date,
-            onChange,
-            mode: currentMode,
-            is24Hour: true,
-        })
-    }
-
-    const showDatepicker = () => {
-        showMode('date')
-    }
-
-    const showTimepicker = () => {
-        showMode('time')
-    }
+    }, [listDreamsByUserForm])
 
     return (
         <Auth>
             <Screen>
-                <Text>DREAMS LIST</Text>
-                <CustomButton title="DATA" onPress={ showDatepicker } />
-                <CustomButton title="TIME" onPress={ showTimepicker } />
+                <DatePicker date={ date } setDate={ setDate } buttonProps={{ title: "Selecione um mês", onPress: () => {} }} />
+                <Picker
+                    selectedValue={ listDreamsByUserForm.dreamCaracteristicsFilter }
+                    onValueChange={ (e) => setListDreamsByUserForm({
+                        ...listDreamsByUserForm,
+                        dreamCaracteristicsFilter: e
+                    })}
+                    style={ styles.dreamCaracteristicsFilterPicker }
+                >
+                    <Picker.Item label="Todos os Sonhos" value="all" />
+                    <Picker.Item label="Todos os Sonhos (menos os ocultos)" value="allNotHidden" />
+                    <Picker.Item label="Todos os Sonhos (menos os eróticos)" value="allNotErotic" />
+                    <Picker.Item label="Todos os Sonho (menos ocultos e eróticos)" value="allNotHiddenAndErotic" />
+                    <Picker.Item label="Todos os Sonhos Ocultos" value="allHidden" />
+                    <Picker.Item label="Todos os Sonhos Eróticos" value="allErotic" />
+                </Picker>
+                <Picker
+                    selectedValue={ listDreamsByUserForm.dreamOriginFilter }
+                    onValueChange={ (e) => setListDreamsByUserForm({
+                        ...listDreamsByUserForm,
+                        dreamOriginFilter: e
+                    })}
+                    style={ styles.dreamOriginFilterPicker }
+                >
+                    <Picker.Item label="Todas as Origens" value="all" />
+                    <Picker.Item label="Sonhos Completos" value="completeDreams" />
+                    <Picker.Item label="Sonhos Rápidos" value="fastDreams" />
+                    <Picker.Item label="Sonhos Importados" value="importedDreams" />
+                </Picker>
+                <View>
+                    <Text>Sem Especificidades</Text>
+                    <Switch
+                        value={ listDreamsByUserForm.dreamEspecificCaracteristicsFilter.noEspecificy }
+                        onValueChange={ (e) => setListDreamsByUserForm({
+                            ...listDreamsByUserForm,
+                            dreamEspecificCaracteristicsFilter: {
+                                ...listDreamsByUserForm.dreamEspecificCaracteristicsFilter,
+                                noEspecificy: e,
+                            }
+                        })}
+                    />
+                </View>
                 {
                     dreamsList
                         ? dreamsList.length > 0
@@ -117,10 +136,6 @@ export const DreamsList: React.FC<DreamsListProps> = ({ route }) => {
                         : <Loading onlyLoading={ false } text="Buscando sonhos..." />
                 }
                 <CustomButton
-                    title="SONHO TESTE"
-                    onPress={ () => dreamsStackNavigation.navigate("GetDream", { id: 1 }) }
-                />
-                <CustomButton
                     title="Voltar"
                     onPress={ () => dreamsStackNavigation.goBack() }
                 />
@@ -130,4 +145,10 @@ export const DreamsList: React.FC<DreamsListProps> = ({ route }) => {
 }
 
 const styles = StyleSheet.create({
+    dreamCaracteristicsFilterPicker: {
+        width: 395
+    },
+    dreamOriginFilterPicker: {
+        width: 230
+    },
 })
--- src/utils/DateFormatter.ts
+++ src/utils/DateFormatter.ts
@@ -2,5 +2,22 @@ export const DateFormatter = {
     /** @example "2024-11-01T00:00:00.000Z" > "2024-11-01" */
     removeTime: (date: string) => {
         return date.split("T")[0]
-    }
+    },
+    /** @example "2024-11-01T00:00:00.000Z" > "00:00:00" */
+    removeDate: (date: string) => {
+        return date.split("T")[1].split(".")[0]
+    },
+    forBackend: {
+        date: (unix?: number) => {
+            let date = new Date()
+            if (unix) date = new Date(unix)
+            const formatted = DateFormatter.removeTime(date.toISOString())
+            return formatted.replace("-", "/").replace("-", "/")
+        },
+        time: (unix?: number) => {
+            let date = new Date()
+            if (unix) date = new Date(unix)
+            return DateFormatter.removeDate(date.toISOString())
+        },
+    },
 }
